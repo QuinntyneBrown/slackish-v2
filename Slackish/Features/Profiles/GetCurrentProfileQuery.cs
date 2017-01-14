f7834f00fdb183f09463f5b29d7cc98d3d101cec
Quinntyne Brown <quinntynebrown@gmail.com>
2017-01-13 21:19:44 -0500

    current profile

--- Slackish/Features/Profiles/GetCurrentProfileQuery.cs
+++ Slackish/Features/Profiles/GetCurrentProfileQuery.cs
@@ -1,6 +1,46 @@
-﻿namespace Slackish.Features.Profiles
+﻿using System.Threading.Tasks;
+using MediatR;
+using Slackish.Data;
+using Slackish.Utilities;
+using System.Data.Entity;
+
+namespace Slackish.Features.Profiles
 {
     public class GetCurrentProfileQuery
     {
+        public class GetCurrentProfileRequest: IAsyncRequest<GetCurrentProfileResponse>
+        {
+            public string Username { get; set; }
+        }
+
+        public class GetCurrentProfileResponse
+        {
+            public ProfileApiModel Profile { get; set; }
+        }
+
+        public class GetCurrentProfileHandler : IAsyncRequestHandler<GetCurrentProfileRequest, GetCurrentProfileResponse>
+        {
+            public GetCurrentProfileHandler(SlackishDbContext slackishDbContext, ICache cache)
+            {
+                _slackishDbContext = slackishDbContext;
+                _cache = cache;
+            }
+
+            public async Task<GetCurrentProfileResponse> Handle(GetCurrentProfileRequest message)
+            {
+                var profile = await _cache.FromCacheOrServiceAsync(() => _slackishDbContext
+                    .Profiles
+                    .SingleAsync(x => x.User.Username == message.Username),"[Profile] CurrentProfile");
+
+                return new GetCurrentProfileResponse()
+                {
+                    Profile = ProfileApiModel.FromProfile(profile)
+                };
+                
+            }
+
+            private SlackishDbContext _slackishDbContext { get; set; }
+            private ICache _cache { get; set; }
+        }
     }
 }
