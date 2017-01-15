40030da7acd652766857615cfaa46510bc781d03
Quinntyne Brown <quinntynebrown@gmail.com>
2017-01-15 02:41:43 -0500

    profile api model

--- Slackish/Features/Profiles/ProfileApiModel.cs
+++ Slackish/Features/Profiles/ProfileApiModel.cs
@@ -1,5 +1,7 @@
 using Slackish.Data.Models;
+using Slackish.Features.Conversations;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Slackish.Features.Profiles
 {
@@ -7,16 +9,21 @@ namespace Slackish.Features.Profiles
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public UserApiModel User { get; set; }
+        public ICollection<ConversationApiModel> Conversations { get; set; } = new HashSet<ConversationApiModel>();
 
         public static TModel FromProfile<TModel>(Profile profile) where
             TModel : ProfileApiModel, new()
         {
             var model = new TModel();
             model.Id = profile.Id;
+            model.User = UserApiModel.FromUser(profile.User);
+            model.Conversations = profile.Conversations.Select(x => ConversationApiModel.FromConversation(x)).ToList();
             return model;
         }
 
         public static ProfileApiModel FromProfile(Profile profile)
             => FromProfile<ProfileApiModel>(profile);
     }
+    
 }
---
+++ Slackish/Features/Profiles/UserApiModel.cs
@@ -0,0 +1,21 @@
+﻿using Slackish.Data.Models;
+
+namespace Slackish.Features.Profiles
+{
+    public class UserApiModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static TModel FromUser<TModel>(User user) where
+            TModel : UserApiModel, new()
+        {
+            var model = new TModel();
+            model.Id = user.Id;
+            return model;
+        }
+
+        public static UserApiModel FromUser(User user)
+            => FromUser<UserApiModel>(user);
+    }
+}
