f2608d653da102a6fb7bfaa8caa97a96e7ad60ce
Quinntyne Brown <quinntynebrown@gmail.com>
2017-01-17 04:36:24 -0500

    GetMessagesQuery

---
+++ Slackish/Features/Messages/GetMessagesQuery.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Slackish.Data;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Linq;
+using Slackish.Utilities;
+
+namespace Slackish.Features.Messages
+{
+    public class GetMessagesQuery: IAsyncRequest<GetMessagesResponse>
+    {
+        public GetMessagesQuery() { }        
+    }
+
+    public class GetMessagesResponse 
+    {
+        public List<MessageApiModel> Messages { get; set; }
+    }
+
+    public class GetMessagesQueryHandler: IAsyncRequestHandler<GetMessagesQuery, GetMessagesResponse>
+    {
+        public GetMessagesQueryHandler(SlackishDbContext dataContext, ICache cache)
+        {
+            _cache = cache;
+            _slackishDbContext = dataContext;
+        }
+
+        public async Task<GetMessagesResponse> Handle(GetMessagesQuery query)
+        {
+            var messages = await _slackishDbContext.Messages
+                .Include(x=>x.Conversation)
+                .Select(x => MessageApiModel.FromMessage(x))
+                .ToListAsync();
+
+            return new GetMessagesResponse()
+            {
+                Messages = messages
+            };
+        }
+
+        private SlackishDbContext _slackishDbContext;
+        private ICache _cache;
+    }
+}
