6760bcb401f7acfa61097cf64dc9911c767d24af
Quinntyne Brown <quinntynebrown@gmail.com>
2017-01-19 02:07:24 -0500

    user

--- Slackish/Features/Profiles/UserApiModel.cs
+++ Slackish/Features/Profiles/UserApiModel.cs
@@ -5,13 +5,25 @@ namespace Slackish.Features.Profiles
     public class UserApiModel
     {
         public int Id { get; set; }
+        public int TenantId { get; set; }
         public string Name { get; set; }
-
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        
         public static TModel FromUser<TModel>(User user) where
             TModel : UserApiModel, new()
         {
             var model = new TModel();
             model.Id = user.Id;
+            model.TenantId = user.TenantId;
+            model.Name = user.Name;
+            model.Email = user.Email;
+            model.Username = user.Username;
+            model.Firstname = user.Firstname;
+            model.Lastname = user.Lastname;            
             return model;
         }
 
