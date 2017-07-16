a6454b0540450995a2770a7dd5c8cec5754f9d82
Quinntyne Brown <quinntynebrown@gmail.com>
2017-07-15 23:23:42 -0400

    header

--- ClientApp/app/shared/components/header.component.ts
+++ ClientApp/app/shared/components/header.component.ts
@@ -2,7 +2,10 @@ import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
     templateUrl: "./header.component.html",
-    styleUrls: ["./header.component.css"],
+    styleUrls: [
+        "../styles/forms.css",
+        "./header.component.css"
+    ],
     selector: "ce-header"
 })
 export class HeaderComponent { }
